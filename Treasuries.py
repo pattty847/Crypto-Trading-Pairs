87527a2a3c2dc2f1d39c4ad1240419ecfe7d39fa
Patrick McDermott <pattty847@gmail.com>
2022-10-16 19:24:22 -0400

    updit

--- Treasuries.py
+++ Treasuries.py
@@ -6,14 +6,15 @@ class Treasuries():
         self.base_url = "https://api.fiscaldata.treasury.gov/services/api/fiscal_service"
         self.endpoints = {
             "avg_interest_rates":"/v2/accounting/od/avg_interest_rates", 
-            "debt_to_penny":"/v2/accounting/od/debt_to_penny"
+            "debt_to_penny":"/v2/accounting/od/debt_to_penny",
+            "interest_cost":"/v2/debt/tror"
         }
 
     def avg_interest_rates(self):
         fields = "record_date,security_type_desc,security_desc,avg_interest_rate_amt,record_fiscal_year"
-        filter_by = 'record_fiscal_year:eq:2022,security_type_desc:in:Marketable'
+        filter_by = 'record_fiscal_year:eq:2022'
 
-        avg_interest_rates_ = f"{self.base_url}{self.endpoints['avg_interest_rates']}?fields={fields}&filter={filter_by}"
+        avg_interest_rates_ = f"{self.base_url}{self.endpoints['avg_interest_rates']}?fields={fields}&filter={filter_by}&page[size]=1000"
         resp = requests.get(avg_interest_rates_).json()
         df = pd.DataFrame(resp['data'])
         print(df)
@@ -28,5 +29,16 @@ class Treasuries():
         df = pd.DataFrame(resp['data'])
         return df
 
+    def interest_cost(self):
+        fields = ""
+        filter_by = 'record_fiscal_year:eq:2022'
+
+        interest_cost = f"{self.base_url}{self.endpoints['interest_cost']}?fields={fields}&filter={filter_by}"
+        resp = requests.get(interest_cost).json()
+        df = pd.DataFrame(resp['data'])
+        print(df.columns.tolist())
+        return df
+
 t = Treasuries()
-print(t.debt_to_penny())
+df = t.avg_interest_rates()
+df.to_csv("avg_interest_rates.csv")
