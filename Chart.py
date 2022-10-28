ecc57dfe3ad5a23dabf4f7d7dc908ff57f1f256d
Patrick McDermott <pattty847@gmail.com>
2022-10-28 02:56:08 -0400

    d

--- Chart.py
+++ Chart.py
@@ -15,9 +15,13 @@ class Charts():
 
     def launch_trade_panel(self, main):
         with dpg.window(label="Trade", tag="trade-window", width=400, height=500, pos=[0, 25]):
+
             with dpg.menu_bar(tag='trade-menu-bar'):
+
                 with dpg.menu(label="Menu"):
+
                     with dpg.menu(label="Settings"):
+
                         self._add_config_options(
                             "trade-window", 3, 
                             "no_title_bar", "no_scrollbar", "menubar", 
@@ -25,6 +29,7 @@ class Charts():
                             "no_close", "no_background", "no_bring_to_front_on_focus"
                         )
             with dpg.group():
+
                 dpg.add_input_float(label="Limit", default_value=20000, format="%.06f", width=-1)
 
             with dpg.table(header_row=False, borders_innerH=True, 
@@ -36,6 +41,7 @@ class Charts():
                 dpg.add_table_column()
 
                 with dpg.table_row(height=50):
+
                     dpg.add_button(label='Buy', height=48, width=-1)
                     dpg.add_button(label='Sell', height=48, width=-1)
                 
@@ -46,12 +52,23 @@ class Charts():
                 dpg.add_table_column()
 
                 with dpg.table_row(height=50):
+
                     dpg.add_button(label='Cancel', height=48, width=-1)
 
 
     def launch_indicator_panel(self):
         with dpg.window(label="Indicator", tag="indicator-window", width=400, height=500, pos=[0, 25]):
+
             indicator_list = ["RSI", "MACD", "MFI", "SMA", "EMA"]
+            with dpg.table(header_row=False):
+
+                for col in range(4):
+                    dpg.add_table_column()
+
+                with dpg.table_row():
+                    for item in indicator_list:
+                        dpg.add_selectable(label=item)
+                
 
 
     def add_chart(self):
@@ -60,30 +77,28 @@ class Charts():
         self.chart_id += 1
 
         with dpg.tab(label=f"{self.settings['last_symbol']}", parent="charts-tab", tag=f"chart-tab-{self.chart_id}"):
-            # with dpg.collapsing_header(label="Sym/TF"):
-            #     with dpg.group(horizontal=True):
-            #         with dpg.group(horizontal=False, width=250):
-            #             dpg.add_input_text(tag=f"symbols-searcher-{self.chart_id}", hint="Search",
-            #                                callback=lambda sender, data: self.searcher(f"symbols-searcher-{self.chart_id}", 
-            #                                f"symbol-{self.chart_id}", self.api.symbols))
-            #             dpg.add_listbox(self.api.symbols, tag=f'symbol-{self.chart_id}', show=True, callback=lambda a, s: self.change_symbol(a, s, self.chart_id))
-                    
-            #         dpg.add_listbox(self.api.timeframes, callback=lambda a, s : self.change_timeframe(a, s, self.chart_id), width=100)
+
             with dpg.group(horizontal=True):
+
                 symbol = dpg.add_selectable(label="Symbol", width=50)
                 timeframe = dpg.add_selectable(label="TF", width=15)
                 date = dpg.add_selectable(label="Date", width=30)
-                indicator = dpg.add_selectable(label="Indicator", callback=self.launch_indicator_panel)
+                dpg.add_selectable(label="Indicator", callback=self.launch_indicator_panel, width=62)
+
                 with dpg.popup(symbol, mousebutton=dpg.mvMouseButton_Left):
+
                     dpg.add_input_text(tag=f"symbols-searcher-{self.chart_id}", hint="Search",
                                             callback=lambda sender, data: self.searcher(f"symbols-searcher-{self.chart_id}", 
                                             f"symbol-{self.chart_id}", self.api.symbols))
+
                     dpg.add_listbox(self.api.symbols, tag=f'symbol-{self.chart_id}', show=True, callback=lambda a, s: self.change_symbol(a, s, self.chart_id))
 
                 with dpg.popup(timeframe, mousebutton=dpg.mvMouseButton_Left):
+
                     dpg.add_listbox(self.api.timeframes, callback=lambda a, s : self.change_timeframe(a, s, self.chart_id), width=100)
 
                 with dpg.popup(date, mousebutton=dpg.mvMouseButton_Left):
+
                     # date = datetime.today().strftime('%Y-%m-%d').split("-")
                     date = self.settings['last_since'].split("T")[0].split("-")
                     year = str(int(date[0][2:]))
@@ -93,20 +108,27 @@ class Charts():
                     dpg.add_date_picker(level=dpg.mvDatePickerLevel_Day, label='From', default_value=since)
 
             with dpg.subplots(2, 1, label="", width=-1, height=-1, link_all_x=True, row_ratios=[1.0, 0.25]):
+
                 with dpg.plot():
+
                     dpg.add_plot_legend()
                     xaxis_candle_tag = f'candle-series-xaxis-{self.chart_id}'
                     dpg.add_plot_axis(dpg.mvXAxis, tag=xaxis_candle_tag, time=True)
+
                     with dpg.plot_axis(dpg.mvYAxis, tag=f'candle-series-yaxis-{self.chart_id}', label="USD"):
+
                         dpg.add_candle_series(dates, opens, closes, lows, highs, tag=f'candle-series-{self.chart_id}', time_unit=self.do.convert_timeframe(self.settings['last_timeframe']))
                         dpg.fit_axis_data(dpg.top_container_stack())
                         dpg.fit_axis_data(xaxis_candle_tag)
                         
                 with dpg.plot():
+
                     dpg.add_plot_legend()
                     xaxis_volume_tag = f'volume-series-xaxis-{self.chart_id}'
                     dpg.add_plot_axis(dpg.mvXAxis, label="Date", tag=xaxis_volume_tag, time=True)
+
                     with dpg.plot_axis(dpg.mvYAxis, label="USD", tag=f'volume-series-yaxis-{self.chart_id}'):
+
                         dpg.add_bar_series(dates, volume, tag=f'volume-series-{self.chart_id}', weight=1)
                         dpg.fit_axis_data(dpg.top_container_stack())
                         dpg.fit_axis_data(xaxis_volume_tag)
@@ -118,22 +140,34 @@ class Charts():
         """
 
         with dpg.child_window(label="Charts", tag="charts-window", parent="main"):
+
             with dpg.tab_bar(tag="charts-tab"):
+
                 dpg.add_tab_button(label="+", trailing=True, callback=self.add_chart)
 
     def draw_menu(self):
+
         with dpg.menu_bar(tag='main-menu-bar', parent="main"):
+
             with dpg.menu(label="Menu"):
+
                 dpg.add_menu_item(label="Charts", callback=self.launch_charts)
                 dpg.add_menu_item(label="Trade", callback=self.launch_trade_panel)
-                dpg.add_menu_item(label="Demo", callback=lambda:demo.show_demo())
+            
+            with dpg.menu(label="DPG"):
+                dpg.add_menu_item(label="Debug", callback=dpg.show_debug)
+                dpg.add_menu_item(label="ImGui", callback=dpg.show_imgui_demo)
+                dpg.add_menu_item(label="ImPlot", callback=dpg.show_implot_demo)
+                dpg.add_menu_item(label="Demo", callback=demo.show_demo)
 
 
             with dpg.menu(label="Save"):
+
                 dpg.add_menu_item(label="Save Window Configurations", callback=lambda: self.save_init())
 
 
     def change_symbol(self, s, app_data, u):
+
         candles = self.api.get_candles(app_data, self.settings['last_timeframe'], self.settings['last_since'])
         dates, opens, closes, lows, highs, volume = self.do.candles_to_list(candles)
         dpg.configure_item(f'candle-series-{u}', dates=dates, opens=opens, closes=closes, highs=highs, lows=lows, time_unit=self.do.convert_timeframe(self.settings['last_timeframe']))
@@ -144,10 +178,11 @@ class Charts():
 
     
     def change_timeframe(self, s, app_data, u):
+
         candles = self.api.get_candles(self.settings['last_symbol'], app_data, self.settings['last_since'])
         dates, opens, closes, lows, highs, volume = self.do.candles_to_list(candles)
+
         dpg.configure_item(f'candle-series-{u}', dates=dates, opens=opens, closes=closes, highs=highs, lows=lows, time_unit=self.do.convert_timeframe(app_data))
-        # dpg.configure_item(f"chart-tab-{u}", label=app_data)
         dpg.fit_axis_data(f'candle-series-yaxis-{u}')
         dpg.fit_axis_data(f'candle-series-xaxis-{u}')
         self.update_settings({"last_timeframe":app_data})
@@ -206,9 +241,12 @@ class Charts():
 
 
     def searcher(self, searcher, result, search_list):
+
         modified_list = []
+
         if dpg.get_value(searcher) == "*":
             modified_list.extend(iter(search_list))
+
         if dpg.get_value(searcher).lower():
             modified_list.extend(item for item in search_list if dpg.get_value(searcher).lower() in item.lower())
 
